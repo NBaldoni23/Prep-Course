56fee03c523ad550d9922dce347c366203e3bdbd
lautaro nahuel <nahuelvacabaldoni@hotmail.com>
2022-09-28 01:40:40 -0300

    nwe attempt git

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "string";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 4 ;
+const nuevoNum = 6 ;
 
 // Crea una variable booleana:
 const nuevoBool = true ;
